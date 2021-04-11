b1c2f44d6196f2c3e8aa76fab659cc9be5f17d22
HILDEBOG\pinzonaf <pinzonaf@globalhitss.com>
2021-04-11 15:35:03 -0500

    Nuevo ws consultar rol por filtros

--- central-server/central/CentralUsuarios/src/main/java/com/project/cafe/CentralUsuarios/service/IRolService.java
+++ central-server/central/CentralUsuarios/src/main/java/com/project/cafe/CentralUsuarios/service/IRolService.java
@@ -2,6 +2,8 @@ package com.project.cafe.CentralUsuarios.service;
 
 import java.util.List;
 
+import com.project.cafe.CentralUsuarios.dto.RequestConsultarRolesDTO;
+import com.project.cafe.CentralUsuarios.dto.ResponseConsultarDTO;
 import com.project.cafe.CentralUsuarios.model.RolTB;
 
 public interface IRolService {
@@ -20,5 +22,12 @@ public interface IRolService {
 	 * Metodo para buscar rol por codigo
 	 */
 	List<RolTB> buscarRolPorCodigo(String codigoRol);
+	
+	/**
+	 * 
+	 * @param filtroRol
+	 * @return
+	 */
+	ResponseConsultarDTO<RolTB> consultarRolesFiltros(RequestConsultarRolesDTO filtroRol);
 
 }
